480c3441577b22607d62b411babc5238b8b230d1
nalderson <69584531+nalderson@users.noreply.github.com>
2020-10-21 08:34:55 +0100

    uploading my final project

---
+++ js/main.js
@@ -0,0 +1,56 @@
+// PLANET FINDER //
+document.getElementById("mercury").onclick = mercury;
+
+function mercury () {
+document.getElementById("img").setAttribute("src" , "imgs/pf-mercury.png"
+);
+}
+
+document.getElementById("venus").onclick = venus;
+
+function venus () {
+document.getElementById("img").setAttribute("src" , "imgs/pf-venus.png"
+);
+}
+
+document.getElementById("earth").onclick = earth;
+
+function earth () {
+document.getElementById("img").setAttribute("src" , "imgs/pf-earth.png"
+);
+}
+
+document.getElementById("mars").onclick = mars;
+
+function mars () {
+document.getElementById("img").setAttribute("src" , "imgs/pf-mars.png"
+);
+}
+
+document.getElementById("jupiter").onclick = jupiter;
+
+function jupiter () {
+document.getElementById("img").setAttribute("src" , "imgs/pf-jupiter.png"
+);
+}
+
+document.getElementById("saturn").onclick = saturn;
+
+function saturn () {
+document.getElementById("img").setAttribute("src" , "imgs/pf-saturn.png"
+);
+}
+
+document.getElementById("uranus").onclick = uranus;
+
+function uranus () {
+document.getElementById("img").setAttribute("src" , "imgs/pf-uranus.png"
+);
+}
+
+document.getElementById("neptune").onclick = neptune;
+
+function neptune () {
+document.getElementById("img").setAttribute("src" , "imgs/pf-neptune.png"
+);
+}
